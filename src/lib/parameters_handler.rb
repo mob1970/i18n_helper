9bfd21730ce182e5be16b456316daebd5926902c
Miquel Oliete <miquel.oliete@sage.com>
2015-05-22 12:40:03 +0200

    First commit.

---
+++ src/lib/parameters_handler.rb
@@ -0,0 +1,14 @@
+class ParametersHandler
+  def self.extract(parameters=[])
+    options = {}
+    parameters.each do |parameter|
+      process_parameter(options
+    end
+  end
+
+  private
+
+  def process_parameter()
+  end
+
+end
---
+++ src/spec/parameters_handler_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+Rspec.describe ParametersHandler do
+  describe '
+end
+
