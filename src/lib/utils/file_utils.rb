053d25144f32aa60c08b03986d1073c607578550
Miquel Oliete <miquel.oliete@sage.com>
2015-05-27 14:11:38 +0200

    FileUtils new method + spec

--- src/lib/utils/file_utils.rb
+++ src/lib/utils/file_utils.rb
@@ -1,7 +1,11 @@
 module Utils
   module FileUtils
-    def home_directory()
+    def self.home_directory()
       File.expand_path('~')
+    end
+
+    def self.file_exist?(file_path)
+      File.exist?(file_path)
     end
   end
 end
--- src/spec/file_utils_spec.rb
+++ src/spec/file_utils_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Utils::FileUtils do
     let(:expected) { File.expand_path('~') }
 
     it "return user's home directory" do
-      expect(Utils::FileUtils::home_directory).to eq(expected)
+      expect(Utils::FileUtils.home_directory).to eq(expected)
+    end
+  end
+
+  context 'file_exists' do
+    it 'returns true if file exists' do
+      expect(Utils::FileUtils.file_exist?('./spec/files/existent_file.txt')).to eq(true)
+    end
+
+    it 'returns false if file exists'  do
+      expect(Utils::FileUtils.file_exist?('./spec/files/non_existent_file.txt')).to eq(false)
     end
   end
 end
